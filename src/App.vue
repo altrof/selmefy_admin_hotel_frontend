cd83f261a7f42a65e172a2856d94190120e8fd68
Aleksandr Trofimov <aleksandr.trofimov@bigbank.ee>
2022-11-13 02:50:59 +0200

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Navbar from "@/components/_organisms/Navbar/Navbar.vue";
-import { useLoggedInStore } from "@/stores/login.js";
+import { useLoggedInStore } from "@/stores/login";
 import { storeToRefs } from "pinia";
 import LoginForm from "@/components/_organisms/LoginForm/LoginForm.vue";
 
@@ -9,22 +9,10 @@ const { loggedIn } = storeToRefs(useLoggedInStore());
 </script>
 
 <template>
-  <div v-if="loggedIn">
-    <Navbar>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/bookings">Bookings</RouterLink>
-      <RouterLink to="/rooms">Rooms</RouterLink>
-      <RouterLink to="/users">Users</RouterLink>
-      <RouterLink to="/special-offers">Special Offers</RouterLink>
-    </Navbar>
-    <RouterView />
-  </div>
-  <div v-else>
-    <LoginForm />
-  </div>
+  <div>TEST</div>
 </template>
 
-<style scoped>
+<style>
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
