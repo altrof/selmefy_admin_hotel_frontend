16a2a9c2ad347d0514f1683b4236c673083de992
Norman Kuusik <normankuusik@gmail.com>
2022-11-24 19:07:51 +0200

    Make all request params dynamically changeable on UserMainView.

--- src/stores/person.js
+++ src/stores/person.js
@@ -2,42 +2,16 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import PersonsAPI from "../services/modules/PersonAPI";
 
-class Person {
-    constructor(idCode, firstName, lastName, dateOfBirth) {
-        this.idCode = idCode;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth
-    }
-}
-
 export const usePersonstore = defineStore("person", () => {
 
     const responseData = ref(null);
     const peopleInBooking = ref({})
-    const pageNumber = ref(null);
-/*
-  PersonsAPI.getAllPersons(pageNumber.value, 2, 'firstName').then((response) => {
-    responseData.value = response;
-  });
-*/
-  async function performRequest(receivedPageNumber) {
-    PersonsAPI.getAllPersons(receivedPageNumber, 2, 'firstName').then((response) => {
+
+  function performRequest(pageNumber, pageSize, orderBy, filterBy, filterValue) {
+    PersonsAPI.getAllPersons(pageNumber, 2, 'firstName').then((response) => {
         responseData.value = response;
       });
   }
-/*
-    function addPersonToBooking(idCode, firstName, lastName, dateOfBirth) {
-        const currentPerson = new Person(idCode, firstName, lastName, dateOfBirth)
-        peopleInBooking.value[idCode] = currentPerson
-        
-        let alertMessage = `Added ${currentPerson.firstName} ${currentPerson.lastName}
-            \n So far we have booked the following people:\n`
-        for (let idCode of Object.keys(peopleInBooking.value)) {
-            alertMessage += `${peopleInBooking.value[idCode].firstName} ${peopleInBooking.value[idCode].lastName}\n`
-        }
-        alert(alertMessage)
-    }
-*/
-    return {responseData, peopleInBooking, pageNumber, performRequest}
+
+    return {responseData, peopleInBooking, performRequest}
 })
--- src/views/main-views/UsersMainView.vue
+++ src/views/main-views/UsersMainView.vue
@@ -2,26 +2,38 @@
 
 import { usePersonstore } from "@/stores/person";
 import { storeToRefs } from "pinia";
+import FormButton from "@/components/_atoms/FormButton/FormButton.vue";
 import BasicTable from "@/components/_atoms/BasicTable/BasicTable.vue";
 import { onMounted } from "@vue/runtime-core";
 
+
 const { performRequest } = usePersonstore();
-let { pageNumber } = storeToRefs(usePersonstore());
 
 let { responseData } = storeToRefs(usePersonstore());
 
+let pageNumber = 0
+let pageSize = 10
+let orderBy = "lastName"
+let filterBy = null
+let filterValue = null
+        
 defineProps({
   myPageNumber: Number,
 });
 
 onMounted(() => {
-    pageNumber = 0
-    performRequest(pageNumber);
+    performRequest(pageNumber, pageSize, orderBy, filterBy, filterValue);
 })
+
+function nextPage() {
+    pageNumber += 1
+    performRequest(pageNumber)
+}
 
 </script>
 
 <template>
+    <FormButton textContent = "click" @click-handler="nextPage()" />
     <!-- This part is only for waiting the request to arrive. -->
     <div v-if="responseData === null"> 
     </div>
